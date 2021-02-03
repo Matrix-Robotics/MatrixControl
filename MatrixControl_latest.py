69a758cefd9de6d8a5248757a1a86194c9701561
Ryan <Ryan@kkitcn.et>
2021-02-03 15:15:23 +0800

    Merge setRC function

--- MatrixControl_latest.py
+++ MatrixControl_latest.py
@@ -26,16 +26,28 @@ class BoardControl:
         self.__buad = buad
         self.__port = serial.Serial(self.portlist[self.__num], buad, timeout=tout)
         # Wait 2 sec for Mini to boot.
+        # TODO
         time.sleep(2)
 
     def register(self, *device_type):
         pass
 
+    def __sendbuff(self, func, para=-1):
+        # TODO: Need to add exception
+        self.__port.write((func + self.__dex2str(para) + "\n").encode())
+
     def setMOTOR(self, motor_num:int, pwm:int):
+        """ Set Motor with specific socket and speed.
+        Args:
+            motor_num (int): 
+                motor_num is corresponding with M1, M2 socket on board.
+            pwm (int): Speed of motor.
+        """
+
         if self.board_type == 'Mini':
             pwm_adjust = 1
         else:
-            # Mico
+            # Micro
             pwm_adjust = 0
 
         if pwm < 0 and pwm > -101:
@@ -43,15 +55,26 @@ class BoardControl:
         elif pwm > -1 and pwm < 101:
             _pwm = pwm+pwm_adjust
         else:
+            # TODO break point
             _pwm = None
             print('pwm is an integer between -101 to 101.')
-
-        if motor_num == 1:
-            self.__sendbuff(MiniP.M1_SET, _pwm)
-        elif motor_num == 2:
-            self.__sendbuff(MiniP.M2_SET, _pwm)
+        
+        if motor_num in (1, 2):
+            _buff = "{}.M{}_SET".format(self.protocol, motor_num)
+            self.__sendbuff(eval(_buff), _pwm)
         else:
-            print('motor_num option: 1 or 2')
+            print('motor_num options: 1 or 2 .')
             return None
 
+    def setRC(self, rc_num, angle):
+        _angle = self.__txEncode(angle)
+        _buff = "{}.RC{}_SET".format(self.protocol, rc_num)
+
+        if self.board_type == 'Mini' and rc_num in range(1, 5):
+            self.__sendbuff(eval(_buff), _angle)
+
+        elif self.board_type == 'Micro' and rc_num in (1, 2):
+            self.__sendbuff(eval(_buff), _angle)
+        else:
+            return None
     
