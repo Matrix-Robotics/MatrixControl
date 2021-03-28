5c5152b6b59117b1dea97017265770aa2f2a3ef6
DCisHurt <roger33669@gmail.com>
2021-03-28 12:47:16 +0800

    merge protocol files

---
+++ Protocol.py
@@ -0,0 +1,44 @@
+MiniP = {
+    "M1_SET": "MINI01",
+    "M2_SET": "MINI02",
+    "RC1_SET": "MINI03",
+    "RC2_SET": "MINI04",
+    "RC3_SET": "MINI05",
+    "RC4_SET": "MINI06",
+    "D1_SET": "MINI07",
+    "D2_SET": "MINI08",
+    "D3_SET": "MINI09",
+    "D4_SET": "MINI0A",
+    "RGB1R_SET": "MINI0B",
+    "RGB1G_SET": "MINI0C",
+    "RGB1B_SET": "MINI0D",
+    "RGB2R_SET": "MINI0E",
+    "RGB2G_SET": "MINI0F",
+    "RGB2B_SET": "MINI10",
+
+    "BTN1_GET": "MINI80",
+    "BTN2_GET": "MINI81",
+    "D1_GET": "MINI82",
+    "D2_GET": "MINI83",
+    "D3_GET": "MINI84",
+    "D4_GET": "MINI85",
+    "A1_GET": "MINI86",
+    "A2_GET": "MINI87",
+    "A3_GET": "MINI88"
+}
+
+MicroP = {
+    "M1_SET": "MICRO01",
+    "M2_SET": "MICRO02",
+    "RC1_SET": "MICRO03",
+    "RC2_SET": "MICRO04",
+    "RCRLS_SET": "MICRO05",
+
+
+    "D1_GET": "MICRO80",
+    "D2_GET": "MICRO81",
+    "A1_GET": "MICRO82",
+    "A2_GET": "MICRO83",
+    "URD1_GET": "MICRO84",
+    "URD2_GET": "MICRO85"
+}
