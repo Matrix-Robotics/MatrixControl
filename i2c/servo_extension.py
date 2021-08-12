d2352380cacb18b42ec91b15ca85541dcfcb50c5
RUI LONG <m61216051116@gmail.com>
2021-08-12 11:58:01 +0800

    Update MatrixControl Protocals

--- i2c/servo_extension.py
+++ i2c/servo_extension.py
@@ -8,11 +8,3 @@ class ServoExtension:
         _angle = self.dev._txEncode(angle)
         _buff = "I2C{}_SETANGLE_CH{}".format(self.i2c_port, channel)
         self.dev._sendbuff(self.dev.protocol[_buff], _angle)
-
-    def ChannelRelease(self, channel):
-        # channel options: from 1 to 8
-        if self.dev.board_type == "Micro":
-            raise ValueError("ChannelRelease only works on MATRIX Mini")
-
-        _buff = "I2C{}_CHRELEASE_CH{}".format(self.i2c_port, channel)
-        self.dev._sendbuff(self.dev.protocol[_buff], 1)
